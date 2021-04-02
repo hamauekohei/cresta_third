0ea979b11ddf74313b778637fe7df5a46eeb2b35
hamauekohei <79969487+hamauekohei@users.noreply.github.com>
2021-04-02 11:54:28 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 $(function(){
-  var imgHeight = $('.kv-wrapper').outerHeight();  //キービジュアルの画像の高さを取得
-  var bgHeight = $('.fv').outerHeight(); //ファーストビューの高さを取得
+  var imgHeight = $('.kv-wrapper').outerHeight();  
+  var bgHeight = $('.fv').outerHeight(); 
   
 
   $(".burger-btn").on('click',function(){
     if( $(window).scrollTop() < imgHeight -50){
-      //ハンバーガーボタンがfvより上にあるとき
-      $('.bar').toggleClass('cross'); //ハンバーガーボタンのラインをクロスさせるCSSを当てたり外したりする
-      $('.header-nav').toggleClass('open'); //ナビゲーションが開くCSSを当てたり外したりする
-      $('.burger-musk').fadeToggle(300); //背景を暗くするマスクをフェードイン・フェードアウトさせる
-      $('body').toggleClass('noscroll'); //ハンバーガーメニューを開いたときにスクロールしないようにする
+
+      $('.bar').toggleClass('cross');
+      $('.header-nav').toggleClass('open'); 
+      $('.burger-musk').fadeToggle(300); 
+      $('body').toggleClass('noscroll'); 
     }else{
-      //ハンバーガーボタンがfvより下にあるとき
-      $(this).toggleClass('black'); //ハンバーガーボタンを黒くしたり白くしたりする
+
+      $(this).toggleClass('black'); 
       $('.bar').toggleClass('cross');
       $('.header-nav').toggleClass('open');
       $('.burger-musk').fadeToggle(300);
